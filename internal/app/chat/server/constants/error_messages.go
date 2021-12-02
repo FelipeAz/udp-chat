4cc53fdebbd0c14f4d204a269732ff0ffae8a2fb
FelipeAz <felipe9_azevedo@hotmail.com>
2021-12-02 00:41:49 -0500

    Added missing server constant error messages

---
+++ internal/app/chat/server/constants/error_messages.go
@@ -0,0 +1,7 @@
+package constants
+
+const (
+	FailedToReadFromBuffer = "Failed to read from buffer"
+	FailedToWriteDeadline  = "Failed to write deadline"
+	FailedToWriteToBuffer  = "Failed to write deadline"
+)
